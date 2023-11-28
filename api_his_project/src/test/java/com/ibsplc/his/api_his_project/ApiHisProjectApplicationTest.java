252b361e24dd971437619a5efdaaae5203040099
BonifaceNoel <bonifacemac658@gmail.com>
2023-11-28 13:50:24 +0530

    spring-starter-application-test-rename-commit

--- api_his_project/src/test/java/com/ibsplc/his/api_his_project/ApiHisProjectApplicationTest.java
+++ api_his_project/src/test/java/com/ibsplc/his/api_his_project/ApiHisProjectApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiHisProjectApplicationTests {
+class ApiHisProjectApplicationTest {
 
 	@Test
 	void contextLoads() {
