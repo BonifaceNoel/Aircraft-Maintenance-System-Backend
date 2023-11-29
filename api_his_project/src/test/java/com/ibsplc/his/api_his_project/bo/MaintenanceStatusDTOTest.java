9cccd7a0ab924d24919fde7f19a283c070fa5a1e
BonifaceNoel <bonifacemac658@gmail.com>
2023-11-29 14:54:14 +0530

    maintenance-dto-junit-test-update

--- api_his_project/src/test/java/com/ibsplc/his/api_his_project/bo/MaintenanceStatusDTOTest.java
+++ api_his_project/src/test/java/com/ibsplc/his/api_his_project/bo/MaintenanceStatusDTOTest.java
@@ -1,6 +1,6 @@
 package com.ibsplc.his.api_his_project.bo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MaintenanceStatusDTOTest {
