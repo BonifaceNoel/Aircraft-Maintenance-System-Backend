eaa0927d85853724f0feccad90408eb63ddf7a9c
BonifaceNoel <bonifacemac658@gmail.com>
2023-11-30 14:44:10 +0530

    spring-boot-main-app-test-junit-5

--- api_his_project/src/test/java/com/ibsplc/his/api_his_project/ApiHisProjectApplicationTest.java
+++ api_his_project/src/test/java/com/ibsplc/his/api_his_project/ApiHisProjectApplicationTest.java
@@ -1,15 +1,13 @@
 package com.ibsplc.his.api_his_project;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
-
-import com.ibsplc.his.api_his_project.ApiHisProjectApplication;
 
 public class ApiHisProjectApplicationTest {
 
