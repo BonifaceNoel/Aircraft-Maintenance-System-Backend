41d08fce173b4ab51ad00e819959422cc9eaced1
BonifaceNoel <bonifacemac658@gmail.com>
2023-11-30 11:57:27 +0530

    controller-junit-test-update-3

--- api_his_project/src/test/java/com/ibsplc/his/api_his_project/controller/MaintenanceControllerTest.java
+++ api_his_project/src/test/java/com/ibsplc/his/api_his_project/controller/MaintenanceControllerTest.java
@@ -1,6 +1,7 @@
 package com.ibsplc.his.api_his_project.controller;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -11,17 +12,23 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ibsplc.his.api_his_project.bo.FlightInfo;
 import com.ibsplc.his.api_his_project.bo.MaintenanceRecord;
 import com.ibsplc.his.api_his_project.bo.MaintenanceStatusDTO;
+import com.ibsplc.his.api_his_project.exceptions.DeleteFlightException;
 import com.ibsplc.his.api_his_project.exceptions.GetFlightException;
 import com.ibsplc.his.api_his_project.exceptions.GetFlightMaintenanceException;
+import com.ibsplc.his.api_his_project.exceptions.GetMaintenanceByIdException;
 import com.ibsplc.his.api_his_project.exceptions.GetMaintenanceException;
 import com.ibsplc.his.api_his_project.exceptions.GetMaintenanceStatusException;
 import com.ibsplc.his.api_his_project.exceptions.NewFlightException;
+import com.ibsplc.his.api_his_project.exceptions.UpdateFlightException;
 import com.ibsplc.his.api_his_project.service.MaintenanceService;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +37,9 @@ public class MaintenanceControllerTest {
 
     @Mock
     MaintenanceService mainService;
+
+    @Autowired
+    MaintenanceController maintenanceController;
 
     @Test
     public void testFlightListAPI() throws GetFlightException {
@@ -85,5 +95,46 @@ public class MaintenanceControllerTest {
         when(mainService.newFlightDetails(anyString(), anyInt(), anyString(), anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(false);
 
+    }
+
+    @Test
+    public void testUpdateFlight() throws UpdateFlightException {
+        // Arrange
+        String aid = "123";
+        FlightInfo flightInfo = new FlightInfo(/* add necessary parameters */);
+        when(mainService.updateFlight(aid, flightInfo)).thenReturn(true);
+
+        // Act
+        ResponseEntity<String> responseEntity = maintenanceController.updateFlight(aid, flightInfo);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals("{\"Update Status\" : \"Success\"}", responseEntity.getBody());
+
+    }
+
+    @Test
+    public void testDeleteFlight() throws DeleteFlightException {
+        // Arrange
+        String aid = "123";
+        when(mainService.deleteFlight(aid)).thenReturn(true);
+
+        // Act
+        ResponseEntity<String> responseEntity = maintenanceController.deleteFlight(aid);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals("{\"Delete Status\" : \"Success\"}", responseEntity.getBody());
+
+    }
+
+    @Test
+    public void testMaintenanceById() throws GetMaintenanceByIdException {
+
+    	String mid = "123";
+    	List<MaintenanceRecord> maintenanceRecord = new ArrayList<>();
+        when(mainService.getRecord(mid)).thenReturn(maintenanceRecord);
+
+        assertNotNull(maintenanceRecord);
     }
 }
