a03c3952639c8e67e49dfce142b73b5f9a53dc24
BonifaceNoel <bonifacemac658@gmail.com>
2023-11-30 15:31:09 +0530

    maintenance-service-test-junit-5

--- api_his_project/src/test/java/com/ibsplc/his/api_his_project/service/MaintenanceServiceTest.java
+++ api_his_project/src/test/java/com/ibsplc/his/api_his_project/service/MaintenanceServiceTest.java
@@ -1,15 +1,15 @@
 package com.ibsplc.his.api_his_project.service;
 
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ibsplc.his.api_his_project.bo.FlightInfo;
@@ -26,5 +26,67 @@ import com.ibsplc.his.api_his_project.mapper.MaintenanceMapper;
 @SpringBootTest
 public class MaintenanceServiceTest {
 
+    @Mock
+    MaintenanceMapper mainMapper;
+
+    @Test
+    public void testGetFlightDetails() throws GetFlightException {
+        // Mock
+        List<FlightInfo> flightList = new ArrayList<>();
+        when(mainMapper.getFlightsRegistered()).thenReturn(flightList);
+
+        // Assert
+        assertNotNull(flightList);
+    }
+
+    @Test
+    public void testGetMaintenanceDetails() throws GetMaintenanceException {
+        // Mock
+        List<MaintenanceRecord> maintenanceList = new ArrayList<>();
+        when(mainMapper.getMaintenanceRegister()).thenReturn(maintenanceList);
+
+        // Assert
+        assertNotNull(maintenanceList);
+    }
+
+    @Test
+    public void testGetFlightsForMaintenance() throws GetFlightMaintenanceException {
+        // Mock
+        List<MaintenanceStatusDTO> flightDamagedList = new ArrayList<>();
+        when(mainMapper.getFlightsUnderMaintenance()).thenReturn(flightDamagedList);
+
+        // Assert
+        assertNotNull(flightDamagedList);
+    }
+
+    @Test
+    public void testGetMaintenanceStatus() throws GetMaintenanceStatusException {
+        // Mock
+        List<MaintenanceStatusDTO> maintenanceStatusList = new ArrayList<>();
+        when(mainMapper.getMaintenanceDetails(anyString())).thenReturn(maintenanceStatusList);
+
+        // Assert
+        assertNotNull(maintenanceStatusList);
+    }
+
+    @Test
+    public void testGetFlight() throws GetFlightByIdException {
+        // Mock
+        List<FlightInfo> flightList = new ArrayList<>();
+        when(mainMapper.getFlight()).thenReturn(flightList);
+
+        // Assert
+        assertNotNull(flightList);
+    }
+
+    @Test
+    public void testGetRecord() throws GetMaintenanceByIdException {
+        // Mock
+        List<MaintenanceRecord> recordList = new ArrayList<>();
+        when(mainMapper.getRecord()).thenReturn(recordList);
+
+        // Assert
+        assertNotNull(recordList);
+    }
 }
 
